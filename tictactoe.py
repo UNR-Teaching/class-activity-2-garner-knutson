8a65533d7bf839862235e264134bef61951f0caf
drednaut <jaredknutson@nevada.unr.edu>
2020-01-28 16:44:55 -0800

    added testing

--- tictactoe.py
+++ tictactoe.py
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
 """ Note: Although the skeleton below is in Python, you may use any programming language you want so long as the language supports object-oriented programming, 
           and you make use of relevant object-oriented design principles.
 """
-~~~~~~~~~~~~
 
 class Board(object):
 
@@ -10,10 +8,10 @@ class Board(object):
         """
         Initializes the Board of size 3x3
         """
-        board = [['_', '_', '_'],
+        self.board = [['_', '_', '_'],
                 ['_', '_', '_'],
                 ['_', '_', '_']]
-        player1 = True
+        self.player1 = True
 
         pass
 
@@ -39,6 +37,33 @@ class Board(object):
 
         pass
 
+    def is_input_len_two(choice):
+        """
+        Returns true if split is lenth 2,
+        else false
+        """
+        if (len(choice.split(',')) == 2):
+            return True
+        else:
+            return False
+
+
+    def validate_user_input(choice):
+        """
+        Takes in a string and returns column and row after validating
+        """
+        # Split the choice by each element
+        choice_l = choice.split(',')
+        #print(f"CHOICE: {choice_l}")
+        # Assign row and column and convert to integer
+        ########### Try to convert to integer
+        row = int(choice_l[0])
+        column = int(choice_l[1])
+
+        #print(f"ROW: {row} COL: {column}")
+        # return column and row as tuple
+        return (column, row)
+
     def play_game(self):
         """
         Takes moves from raw_input as comma-separated list in form (column, row, player)
@@ -46,8 +71,10 @@ class Board(object):
         
         :return: (str) the letter representing the player who won
         """
-        while not has_winner():
-            choice = input(f"Player {"1" if player1 else "2"} Make your move (row,col): ")
+        while not self.has_winner():
+            choice = input(f"Player {'1' if self.player1 else '2'} Make your move (row,col): ")
+            (column, row) = validate_user_input(choice)
+            #self.mark_square(column, row, player)
          
         pass
         
@@ -55,54 +82,3 @@ if __name__ == '__main__':
     board = Board()
     winner = board.play_game()
     print("{} has won!".format(winner))
-=======
-""" Note: Although the skeleton below is in Python, you may use any programming language you want so long as the language supports object-oriented programming, 
-          and you make use of relevant object-oriented design principles.
-"""
-
-class Board(object):
-
-    def __init__(self):
-        """
-        Initializes the Board of size 3x3
-        """
-
-        pass
-
-    def mark_square(self, column, row, player):
-        """
-        Marks a square at coordinate (column, row) for player
-
-        :param column: (int) the 0-indexed column of the Board to mark
-        :param row: (int) the 0-indexed row of the Board to mark
-        :param player: (str) the X or O representation of which player to mark in square
-
-        :return: ????
-        """
-
-        pass
-
-    def has_winner(self):
-        """
-        Checks to see if there is a current winner of the game
-
-        :return: ????
-        """
-
-        pass
-
-    def play_game(self):
-        """
-        Takes moves from raw_input as comma-separated list in form (column, row, player)
-            and makes a move. When a winner has been determined, the game ends
-        
-        :return: (str) the letter representing the player who won
-        """
-        
-        pass
-        
-if __name__ == '__main__':
-    board = Board()
-    winner = board.play_game()
-    print("{} has won!".format(winner))
->>>>>>> 425cef987c03595443681167df434936057631ae
--- tictactoe_test.py
+++ tictactoe_test.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 import unittest
+from tictactoe import Board
 
-class TestBoard(unittest.TestCase):
+class TestValidInput(unittest.TestCase):
+
+    def test_validate_user_input(self):
+        self.assertEqual(Board.validate_user_input("1,1"), (1,1))
+
+    def test_is_input_len_two(self):
+        self.assertTrue(Board.is_input_len_two("1,3"))
+        self.assertFalse(Board.is_input_len_two("1,3,1"))
+        self.assertFalse(Board.is_input_len_two("1"))
+
+
+if __name__ == '__main__':
+    unittest.main()
