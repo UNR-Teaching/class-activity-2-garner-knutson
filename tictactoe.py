4ac9597c5733deb4577208c093f4b604d65975a5
will <williamgarner@nevada.unr.edu>
2020-01-28 16:02:15 -0800

    no break anymore

--- tictactoe.py
+++ tictactoe.py
@@ -1,7 +1,6 @@
 """ Note: Although the skeleton below is in Python, you may use any programming language you want so long as the language supports object-oriented programming, 
           and you make use of relevant object-oriented design principles.
 """
-~~~~~~~~~~~~
 
 class Board(object):
 
