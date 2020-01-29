e47948ea26ddb1420289589ee1ce24321e8278a8
will <williamgarner@nevada.unr.edu>
2020-01-28 16:49:54 -0800

    merge

--- tictactoe.py
+++ tictactoe.py
@@ -1,6 +1,7 @@
-""" Note: Although the skeleton below is in Python, you may use any programming language you want so long as the language supports object-oriented programming, 
+""" Note: Although the skeleton below is in Python, you may use any programming language you want so long as the language supports object-oriented programming,
           and you make use of relevant object-oriented design principles.
 """
+
 
 class Board(object):
 
@@ -9,8 +10,8 @@ class Board(object):
         Initializes the Board of size 3x3
         """
         self.board = [['_', '_', '_'],
-                ['_', '_', '_'],
-                ['_', '_', '_']]
+                      ['_', '_', '_'],
+                      ['_', '_', '_']]
         self.player1 = True
 
         pass
@@ -28,25 +29,42 @@ class Board(object):
 
         pass
 
-    def has_winner(self):
+    def has_three_in_a_row(self):
         """
-        Checks to see if there is a current winner of the game
+                Checks to see if there is a current winner of the game
 
-        :return: ????
-        """
+                :return: 1 if player 1 has won, 2 if player 2 has won or 0 if no one has won
+                """
+
+        # Check for three in a row in the rows
+        for row in self.board:
+            if row[0] == row[1] == row[2] and row[0] != '_':
+                return 1 if row[0] == 'X' else 2
+
+        # Check for 3 in a row in a column
+        for column in list(map(list, zip(*self.board))):
+            if column[0] == column[1] == column[2] and column[0] != '_':
+                return 1 if column[0] == 'X' else 2
+
+        # Check for 3 in a row in a diagonal
+        if self.board[0][0] == self.board[1][1] == self.board[2][2] and board[0][0] != '_':
+            return 1 if board[0][0] == 'X' else 2
+        elif self.board[0][2] == self.board[1][1] == board[2][0] and board[0][2] != '_':
+            return 1 if board[0][0] == 'X' else 2
+        else:
+            return 0
+
+    def has_winner(self):
 
         pass
 
     def is_input_len_two(choice):
         """
+        :param choice: (str)
         Returns true if split is lenth 2,
         else false
         """
-        if (len(choice.split(',')) == 2):
-            return True
-        else:
-            return False
-
+        return len(choice.split(',')) == 2
 
     def validate_user_input(choice):
         """
@@ -54,13 +72,13 @@ class Board(object):
         """
         # Split the choice by each element
         choice_l = choice.split(',')
-        #print(f"CHOICE: {choice_l}")
+        # print(f"CHOICE: {choice_l}")
         # Assign row and column and convert to integer
         ########### Try to convert to integer
         row = int(choice_l[0])
         column = int(choice_l[1])
 
-        #print(f"ROW: {row} COL: {column}")
+        # print(f"ROW: {row} COL: {column}")
         # return column and row as tuple
         return (column, row)
 
@@ -73,11 +91,10 @@ class Board(object):
         """
         while not self.has_winner():
             choice = input(f"Player {'1' if self.player1 else '2'} Make your move (row,col): ")
-            (column, row) = validate_user_input(choice)
-            #self.mark_square(column, row, player)
-         
+
         pass
-        
+
+
 if __name__ == '__main__':
     board = Board()
     winner = board.play_game()
